c99ab4e6b1f94a656f3c80cd252a6bf9001a3811
Flynn Platt <flynn.platt@jpl.nasa.gov>
2019-05-28 13:58:39 -0700

    catching files

---
+++ src/reducers/reducerFunctions/ViewReducer.js
@@ -0,0 +1,6 @@
+import ViewReducerCore from "_core/reducers/reducerFunctions/ViewReducer";
+import MiscUtil from "utils/MiscUtil";
+
+export default class ViewReducer extends ViewReducerCore {
+    static miscUtil = MiscUtil;
+}
